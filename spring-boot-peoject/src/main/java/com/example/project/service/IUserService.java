c2fa08d521daf2f952286bbf61435bd680df19a4
MengZhao <m235zhao@uwaterloo.ca>
2021-01-31 02:14:29 -0500

    delete peoject

--- spring-boot-peoject/src/main/java/com/example/project/service/IUserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.project.service;
-
-import com.example.project.entity.User;
-
-public interface IUserService {
-    String getpassword(String username);
-
-    User getuser(String username);
-}
