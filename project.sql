67cd5faadce327b5ad770f6d526e1342c46b8455
MengZhao <m235zhao@uwaterloo.ca>
2021-02-19 00:49:01 -0500

    db updated

--- project.sql
+++ project.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80011
  File Encoding         : 65001
 
- Date: 17/02/2021 10:17:00
+ Date: 19/02/2021 00:47:20
 */
 
 SET NAMES utf8mb4;
@@ -54,7 +54,7 @@ CREATE TABLE `comment_reply`  (
   CONSTRAINT `comment_reply_ibfk_2` FOREIGN KEY (`from_user`) REFERENCES `user_info` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `comment_reply_ibfk_3` FOREIGN KEY (`to_user`) REFERENCES `user_info` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `comment_reply_ibfk_4` FOREIGN KEY (`comment_id`) REFERENCES `post_comment` (`comment_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of comment_reply
@@ -62,6 +62,8 @@ CREATE TABLE `comment_reply`  (
 INSERT INTO `comment_reply` VALUES (1, 3, 'I agree', '2021-02-17 05:25:47', 'meng', 'Mao');
 INSERT INTO `comment_reply` VALUES (2, 4, 'no', '2021-02-17 05:29:57', 'meng', 'ppp');
 INSERT INTO `comment_reply` VALUES (4, 3, 'hahahaha', '2021-02-17 12:48:34', 'Mao', 'meng');
+INSERT INTO `comment_reply` VALUES (5, 8, 'reply test', '2021-02-18 16:27:17', 'Mao', 'Mao');
+INSERT INTO `comment_reply` VALUES (7, 8, 'string,,,', '2021-02-18 16:29:33', 'ppp', 'Mao');
 
 -- ----------------------------
 -- Table structure for module
@@ -77,6 +79,7 @@ CREATE TABLE `module`  (
 -- ----------------------------
 -- Records of module
 -- ----------------------------
+INSERT INTO `module` VALUES ('java', 'Java Is the Language of Possibilities. Java is powering the innovation behind our digital world. Harness this potential with Java resources for student coders, hobbyists, developers, and IT leaders.', 5);
 INSERT INTO `module` VALUES ('life', 'life 123', 5);
 INSERT INTO `module` VALUES ('springboot', 'Spring Boot offers a fast way to build applications. It looks at your classpath and at the beans you have configured, makes reasonable assumptions about what you are missing, and adds those items. With Spring Boot, you can focus more on business features and less on infrastructure.', 5);
 INSERT INTO `module` VALUES ('vue', 'Vue (pronounced /vjuː/, like view) is a progressive framework for building user interfaces. It is designed from the ground up to be incrementally adoptable, and can easily scale between a library and a framework depending on different use cases. It consists of an approachable core library that focuses on the view layer only, and an ecosystem of supporting libraries that helps you tackle complexity in large Single-Page Applications.', 5);
@@ -91,22 +94,30 @@ CREATE TABLE `post`  (
   `post_content` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
   `post_author` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `post_createtime` datetime(0) NULL DEFAULT NULL,
-  `post_likes` int(11) NULL DEFAULT NULL COMMENT 'number of like',
-  `post_comments` int(11) NULL DEFAULT NULL COMMENT 'number of comment',
-  `post_collections` int(11) NULL DEFAULT NULL COMMENT 'number of collection',
+  `post_likes` int(11) NULL DEFAULT 0 COMMENT 'number of like',
+  `post_comments` int(11) NULL DEFAULT 0 COMMENT 'number of comment',
+  `post_collections` int(11) NULL DEFAULT 0 COMMENT 'number of collection',
   PRIMARY KEY (`post_id`) USING BTREE,
   INDEX `author`(`post_author`) USING BTREE,
   INDEX `module_name`(`module_name`) USING BTREE,
   CONSTRAINT `post_ibfk_2` FOREIGN KEY (`post_author`) REFERENCES `user_info` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `post_ibfk_3` FOREIGN KEY (`module_name`) REFERENCES `module` (`module_name`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of post
 -- ----------------------------
 INSERT INTO `post` VALUES (2, 'springboot', 'springboot is perfect', 'meng', '2021-02-06 03:28:00', 2, 3, 0);
-INSERT INTO `post` VALUES (3, 'vue', 'vue vuevue vue', 'ppp', '2021-02-06 03:28:24', 0, 0, 0);
+INSERT INTO `post` VALUES (3, 'vue', 'vue vuevue vue', 'ppp', '2021-02-06 03:28:24', 1, 0, 0);
 INSERT INTO `post` VALUES (4, NULL, 'Test', 'meng', '2021-02-06 08:31:57', 0, 0, 1);
+INSERT INTO `post` VALUES (5, 'java', 'System.out.println(\"hello java\")', 'Mao', '2021-02-18 03:30:57', 0, 0, 0);
+INSERT INTO `post` VALUES (6, 'java', 'java is the best programming language', 'meng', '2021-02-18 03:32:24', 0, 0, 0);
+INSERT INTO `post` VALUES (7, 'springboot', 'spspspspsp', 'Mao', '2021-02-18 05:31:19', 3, 0, 0);
+INSERT INTO `post` VALUES (8, NULL, 'life hahaha', 'meng', '2021-02-18 11:15:23', 2, 2, 0);
+INSERT INTO `post` VALUES (9, NULL, 'sohai', 'ppp', '2021-02-18 11:15:43', 3, 0, 0);
+INSERT INTO `post` VALUES (10, NULL, 'zzy', 'Mao', '2021-02-18 11:15:56', 0, 0, 0);
+INSERT INTO `post` VALUES (11, NULL, 'today is happy', 'meng', '2021-02-19 00:20:37', 1, 0, 0);
+INSERT INTO `post` VALUES (12, NULL, 'wuhu', 'ppp', '2021-02-19 00:20:53', 0, 0, 0);
 
 -- ----------------------------
 -- Table structure for post_collect
@@ -115,8 +126,8 @@ DROP TABLE IF EXISTS `post_collect`;
 CREATE TABLE `post_collect`  (
   `post_id` int(11) NOT NULL,
   `collect_user` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  INDEX `post_id`(`post_id`) USING BTREE,
   INDEX `collect_user`(`collect_user`) USING BTREE,
+  INDEX `post_id`(`post_id`) USING BTREE,
   CONSTRAINT `post_collect_ibfk_2` FOREIGN KEY (`collect_user`) REFERENCES `user_info` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `post_collect_ibfk_3` FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
@@ -137,11 +148,11 @@ CREATE TABLE `post_comment`  (
   `comment_content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `comment_time` datetime(0) NULL DEFAULT NULL,
   PRIMARY KEY (`comment_id`) USING BTREE,
-  INDEX `post_id`(`post_id`) USING BTREE,
   INDEX `comment_user`(`comment_user`) USING BTREE,
+  INDEX `post_id`(`post_id`) USING BTREE,
   CONSTRAINT `post_comment_ibfk_2` FOREIGN KEY (`comment_user`) REFERENCES `user_info` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `post_comment_ibfk_3` FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of post_comment
@@ -149,6 +160,8 @@ CREATE TABLE `post_comment`  (
 INSERT INTO `post_comment` VALUES (3, 2, 'Mao', 'good post', '2021-02-17 05:18:44');
 INSERT INTO `post_comment` VALUES (4, 2, 'ppp', 'what a bad post!', '2021-02-17 05:19:20');
 INSERT INTO `post_comment` VALUES (5, 2, 'meng', 'I like my post', '2021-02-17 10:36:07');
+INSERT INTO `post_comment` VALUES (8, 8, 'Mao', 'like like like', '2021-02-18 16:22:14');
+INSERT INTO `post_comment` VALUES (9, 8, 'ppp', 'like like like', '2021-02-18 16:24:12');
 
 -- ----------------------------
 -- Table structure for post_like
@@ -169,6 +182,16 @@ CREATE TABLE `post_like`  (
 -- ----------------------------
 INSERT INTO `post_like` VALUES (2, 'Mao', '2021-02-16 22:56:25');
 INSERT INTO `post_like` VALUES (2, 'meng', '2021-02-16 22:56:41');
+INSERT INTO `post_like` VALUES (3, 'meng', '2021-02-18 05:02:39');
+INSERT INTO `post_like` VALUES (7, 'Mao', '2021-02-18 05:33:07');
+INSERT INTO `post_like` VALUES (7, 'meng', '2021-02-18 05:32:58');
+INSERT INTO `post_like` VALUES (7, 'ppp', '2021-02-18 05:32:45');
+INSERT INTO `post_like` VALUES (8, 'Mao', '2021-02-18 11:16:17');
+INSERT INTO `post_like` VALUES (8, 'meng', '2021-02-18 11:16:34');
+INSERT INTO `post_like` VALUES (9, 'Mao', '2021-02-18 16:18:39');
+INSERT INTO `post_like` VALUES (9, 'meng', '2021-02-18 16:19:14');
+INSERT INTO `post_like` VALUES (9, 'ppp', '2021-02-18 16:18:55');
+INSERT INTO `post_like` VALUES (11, 'Mao', '2021-02-19 05:21:26');
 
 -- ----------------------------
 -- Table structure for user_follow
